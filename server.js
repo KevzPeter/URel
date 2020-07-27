cfc3c5792a07e4dfd21e9a285ed86ac779b3f08b
Kevin Peter <29401940+KevzPeter@users.noreply.github.com>
2020-07-27 22:36:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(cors())
 if(process.env.NODE_ENV==='production')
 {
  app.use(express.static(path.join(__dirname, './client/build')))
+ app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.post('/shortUrls', async (req, res) => {
